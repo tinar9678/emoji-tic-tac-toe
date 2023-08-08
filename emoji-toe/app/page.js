773adfe85dd03d06b4f81fe38099658b00859e8e
tinar9678 <tina.r9678@gmail.com>
2023-08-08 15:50:17 -0400

    Adds title

--- emoji-toe/app/page.js
+++ emoji-toe/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        hello
+        	&#129321;&#128515; emoji toe (a game of tic-tac-toe with emojis) &#128518;&#128561;
       </div>
     </main>
   )
